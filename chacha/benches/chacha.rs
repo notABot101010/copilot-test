ca470242bd0b4886f83ea44f52f7e883ae4b566d
git <>
2025-12-03 07:25:59 +0000

    Add chacha20_force_avx2 flag

--- chacha/benches/chacha.rs
+++ chacha/benches/chacha.rs
@@ -80,71 +80,71 @@ fn bench_chacha_vs_crate(c: &mut Criterion) {
     group.finish();
 }
 
-fn bench_chacha_partial_blocks(c: &mut Criterion) {
-    let key = [0u8; 32];
-    let nonce = [0u8; 8];
+// fn bench_chacha_partial_blocks(c: &mut Criterion) {
+//     let key = [0u8; 32];
+//     let nonce = [0u8; 8];
 
-    let mut group = c.benchmark_group("chacha_partial_blocks");
+//     let mut group = c.benchmark_group("chacha_partial_blocks");
 
-    // Benchmark partial block handling
-    for chunk_size in [1, 7, 15, 31, 63].iter() {
-        group.bench_with_input(
-            BenchmarkId::new("ChaCha20", chunk_size),
-            chunk_size,
-            |b, &chunk_size| {
-                let mut data = vec![0u8; 1024];
-                b.iter(|| {
-                    let mut cipher = ChaCha20::new(black_box(&key), black_box(&nonce));
-                    for chunk in data.chunks_mut(chunk_size) {
-                        cipher.xor_keystream(black_box(chunk));
-                    }
-                });
-            },
-        );
-    }
+//     // Benchmark partial block handling
+//     for chunk_size in [1, 7, 15, 31, 63].iter() {
+//         group.bench_with_input(
+//             BenchmarkId::new("ChaCha20", chunk_size),
+//             chunk_size,
+//             |b, &chunk_size| {
+//                 let mut data = vec![0u8; 1024];
+//                 b.iter(|| {
+//                     let mut cipher = ChaCha20::new(black_box(&key), black_box(&nonce));
+//                     for chunk in data.chunks_mut(chunk_size) {
+//                         cipher.xor_keystream(black_box(chunk));
+//                     }
+//                 });
+//             },
+//         );
+//     }
 
-    group.finish();
-}
+//     group.finish();
+// }
 
-fn bench_chacha_block_generation(c: &mut Criterion) {
-    let key = [0u8; 32];
-    let nonce = [0u8; 8];
+// fn bench_chacha_block_generation(c: &mut Criterion) {
+//     let key = [0u8; 32];
+//     let nonce = [0u8; 8];
 
-    let mut group = c.benchmark_group("chacha_block_generation");
+//     let mut group = c.benchmark_group("chacha_block_generation");
 
-    group.bench_function("ChaCha8_single_block", |b| {
-        let mut data = [0u8; 64];
-        b.iter(|| {
-            let mut cipher = ChaCha8::new(black_box(&key), black_box(&nonce));
-            cipher.xor_keystream(black_box(&mut data));
-        });
-    });
+//     group.bench_function("ChaCha8_single_block", |b| {
+//         let mut data = [0u8; 64];
+//         b.iter(|| {
+//             let mut cipher = ChaCha8::new(black_box(&key), black_box(&nonce));
+//             cipher.xor_keystream(black_box(&mut data));
+//         });
+//     });
 
-    group.bench_function("ChaCha12_single_block", |b| {
-        let mut data = [0u8; 64];
-        b.iter(|| {
-            let mut cipher = ChaCha12::new(black_box(&key), black_box(&nonce));
-            cipher.xor_keystream(black_box(&mut data));
-        });
-    });
+//     group.bench_function("ChaCha12_single_block", |b| {
+//         let mut data = [0u8; 64];
+//         b.iter(|| {
+//             let mut cipher = ChaCha12::new(black_box(&key), black_box(&nonce));
+//             cipher.xor_keystream(black_box(&mut data));
+//         });
+//     });
 
-    group.bench_function("ChaCha20_single_block", |b| {
-        let mut data = [0u8; 64];
-        b.iter(|| {
-            let mut cipher = ChaCha20::new(black_box(&key), black_box(&nonce));
-            cipher.xor_keystream(black_box(&mut data));
-        });
-    });
+//     group.bench_function("ChaCha20_single_block", |b| {
+//         let mut data = [0u8; 64];
+//         b.iter(|| {
+//             let mut cipher = ChaCha20::new(black_box(&key), black_box(&nonce));
+//             cipher.xor_keystream(black_box(&mut data));
+//         });
+//     });
 
-    group.finish();
-}
+//     group.finish();
+// }
 
 criterion_group!(
     benches,
     bench_chacha_encrypt,
     bench_chacha_vs_crate,
-    bench_chacha_partial_blocks,
-    bench_chacha_block_generation,
+    // bench_chacha_partial_blocks,
+    // bench_chacha_block_generation,
 );
 
 criterion_main!(benches);
