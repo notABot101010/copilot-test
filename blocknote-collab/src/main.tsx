6964d4ca26be5c1abb8f9560e6984faafce6727d
git <>
2025-12-11 09:51:59 +0000

    collab

---
+++ blocknote-collab/src/main.tsx
@@ -0,0 +1,118 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { BlockNoteView } from "@blocknote/mantine";
+import { useCreateBlockNote } from "@blocknote/react";
+import * as Y from "yjs";
+import * as Automerge from "@automerge/automerge";
+
+import './index.css';
+import '@mantine/core/styles.css';
+import "@blocknote/mantine/blocknoteStyles.css";
+import { MantineProvider } from "@mantine/core";
+import { WebrtcProvider } from "y-webrtc";
+import { IndexeddbPersistence } from "y-indexeddb";
+import { applyBlockNoteChanges, createBlockNoteDocument, type BlockNoteDocument } from "@copilot-test/automerge-utils";
+
+const ydoc = new Y.Doc();
+
+class YProvider {
+  private doc: Y.Doc;
+  private name: string;
+
+  constructor(doc: Y.Doc, name = "YProvider") {
+    this.doc = doc;
+    this.name = name;
+
+    this.doc.on("update", this.onLocalUpdate);
+
+    console.log(`[${this.name}] provider started`);
+  }
+
+  private onLocalUpdate = (update: Uint8Array, origin: any) => {
+    Y.applyUpdate(this.doc, update);
+  };
+
+  public receiveUpdate(update: Uint8Array): void {
+    Y.applyUpdate(this.doc, update, `${this.name}-external`);
+  }
+
+  public destroy(): void {
+    this.doc.off("update", this.onLocalUpdate);
+  }
+}
+
+// const yProvider = new YProvider(ydoc);
+const yProvider = new WebrtcProvider("my-document-id", ydoc);
+// const yProvider = new IndexeddbPersistence('BLOCKNOTE_INDEXED', ydoc)
+
+const DOCUMENT_LOCAL_STORAGE_KEY = 'BLOCKNOTE_COLLAB_AUTOMERGE_DOC';
+
+function loadOrCreateAutomergeDoc(): Automerge.Doc<BlockNoteDocument<any, any, any>> {
+  let savedDoc = localStorage.getItem(DOCUMENT_LOCAL_STORAGE_KEY);
+
+  if (savedDoc) {
+    const t0 = performance.now();
+    const doc = Automerge.load(base64ToUint8Array(savedDoc));
+    const t1 = performance.now();
+    console.log(`Automerge document loaded in ${t1 - t0} ms`)
+    return doc as any;
+  }
+  return Automerge.from({
+    blocks: [],
+  }) as any;
+}
+
+let automergeDoc = loadOrCreateAutomergeDoc();
+
+
+
+function App() {
+  const editor = useCreateBlockNote({
+    initialContent: automergeDoc.blocks.length === 0 ? undefined : automergeDoc.blocks,
+    // collaboration: {
+    //   provider: yProvider,
+    //   fragment: ydoc.getXmlFragment("blocks"),
+    //   user: {
+    //     name: "My Username",
+    //     color: "#ff0000",
+    //   },
+    // },
+  });
+
+  editor.onChange((editor, { getChanges }) => {
+      const changes = getChanges();
+      automergeDoc = applyBlockNoteChanges(automergeDoc, changes);
+      console.log(`Automerge Doc size: ${Automerge.save(automergeDoc).length}`);
+      console.log(`Yjs Doc size: ${Y.encodeStateAsUpdateV2(ydoc).length}`)
+      localStorage.setItem(DOCUMENT_LOCAL_STORAGE_KEY, uint8ArrayToBase64(Automerge.save(automergeDoc)));
+  });
+
+  return <BlockNoteView editor={editor} />;
+}
+
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    {/* <MantineProvider> */}
+      <App />
+    {/* </MantineProvider> */}
+  </StrictMode>,
+)
+
+
+  function base64ToUint8Array(base64: string): Uint8Array {
+    const binary = atob(base64);
+    const bytes = new Uint8Array(binary.length);
+    for (let idx = 0; idx < binary.length; idx += 1) {
+      bytes[idx] = binary.charCodeAt(idx);
+    }
+    return bytes;
+  }
+
+  function uint8ArrayToBase64(bytes: Uint8Array): string {
+    let binary = "";
+    for (let idx = 0; idx < bytes.length; idx += 1) {
+      binary += String.fromCharCode(bytes[idx]);
+    }
+    return btoa(binary);
+  }
